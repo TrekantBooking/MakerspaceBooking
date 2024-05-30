7c81ad3302d3f59b10314b3819224a67f1c12850
hckjensen <hckjensen96@gmail.com>
2024-05-30 14:33:54 +0200

    dqwqw

--- TrekantenBookingSystem/src/App.jsx
+++ TrekantenBookingSystem/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import style from './App.module.scss'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const machines = [
+    "Bambu Lab",
+    "Brother",
+    "Eduard",
+    "VLS 3.50",
+    "VLS 6.60",
+    "PRO 4100S",
+    "Roland BIV-20"
+  ]
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Book en maskine </h1>
+      <div className={style.machineList}>
+        {machines.map((machine, index) => (
+          <div key={index} >
+            <h2>{machine}</h2>
+            <div className={style.buttonContainer}>
+              <button>Book</button>
+
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
