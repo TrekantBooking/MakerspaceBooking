892bf67fafa4ba7782e20365b88284d1b51dcf89
hckjensen <hckjensen96@gmail.com>
2024-05-31 12:34:47 +0200

    booking

--- TrekantenBookingSystem/src/components/bookings/bookings.jsx
+++ TrekantenBookingSystem/src/components/bookings/bookings.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react'
 import style from './bookings.module.scss'
 import { createClient } from '@supabase/supabase-js'
+import PropTypes from 'prop-types';
 
 const supabaseUrl = 'https://kakelsuvivlhklklbwpy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtha2Vsc3V2aXZsaGtsa2xid3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNjk2ODAsImV4cCI6MjAzMjY0NTY4MH0.uzudZASPyKYTrYHOkKQHmUiXgNIaCJ98Nwn-NaWrmkQ'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 
-const Bookings = () => {
+const Bookings = ({ machineId }) => {
 
     const [bookings, setBookings] = useState([])
 
@@ -20,6 +21,7 @@ const Bookings = () => {
             const { data, error } = await supabase
                 .from('bookings')
                 .select('*')
+                .eq('machine_id', machineId)
 
             console.log('Data:', data)
             console.log('Error:', error)
@@ -29,14 +31,29 @@ const Bookings = () => {
         }
 
         fetchBookings()
-    }, [])
+    }, [machineId])
 
     return (
         <>
+
+            <div className={style.bookingList}>
+                {bookings.map((booking, index) => (
+                    <div key={index} >
+                        <h2>{booking.user_name}</h2> {/* Assuming each booking object has a 'name' property */}
+
+                    </div>
+                ))}
+            </div>
 
         </>
 
     )
 }
+
+Bookings.propTypes = {
+    machineId: PropTypes.string.isRequired,
+};
+
+
 
 export default Bookings;
--- TrekantenBookingSystem/src/components/machines/machines.jsx
+++ TrekantenBookingSystem/src/components/machines/machines.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import style from './machines.module.scss'
 import { createClient } from '@supabase/supabase-js'
+import Bookings from '../bookings/bookings'
 
 const supabaseUrl = 'https://kakelsuvivlhklklbwpy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtha2Vsc3V2aXZsaGtsa2xid3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNjk2ODAsImV4cCI6MjAzMjY0NTY4MH0.uzudZASPyKYTrYHOkKQHmUiXgNIaCJ98Nwn-NaWrmkQ'
@@ -37,6 +38,9 @@ const Machines = () => {
                 {machines.map((machine, index) => (
                     <div key={index} >
                         <h2>{machine.name}</h2> {/* Assuming each machine object has a 'name' property */}
+                        <div className={style.bookingList}>
+                            <Bookings machineId={machine.id} />
+                        </div>
                         <div className={style.buttonContainer}>
                             <button>Book</button>
                         </div>
