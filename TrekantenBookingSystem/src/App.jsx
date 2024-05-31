8d1d70f5290568bcde7058eada409714c3d7fa85
hckjensen <hckjensen96@gmail.com>
2024-05-31 10:02:55 +0200

    fddgdg

--- TrekantenBookingSystem/src/App.jsx
+++ TrekantenBookingSystem/src/App.jsx
@@ -1,19 +1,39 @@
 import { useState, useEffect } from 'react'
 import style from './App.module.scss'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kakelsuvivlhklklbwpy.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtha2Vsc3V2aXZsaGtsa2xid3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwNjk2ODAsImV4cCI6MjAzMjY0NTY4MH0.uzudZASPyKYTrYHOkKQHmUiXgNIaCJ98Nwn-NaWrmkQ'
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 
 function App() {
 
-  const machines = [
-    "Bambu Lab",
-    "Brother",
-    "Eduard",
-    "VLS 3.50",
-    "VLS 6.60",
-    "PRO 4100S",
-    "Roland BIV-20"
-  ]
+
+  const [machines, setMachines] = useState([])
+
+
+
+
+
+  useEffect(() => {
+    const fetchMachines = async () => {
+      const { data, error } = await supabase
+        .from('machines')
+        .select('*')
+
+      console.log('Data:', data)
+      console.log('Error:', error)
+
+      if (error) console.error('Error fetching machines:', error)
+      else setMachines(data)
+    }
+
+    fetchMachines()
+  }, [])
+
+
 
 
   return (
@@ -22,10 +42,9 @@ function App() {
       <div className={style.machineList}>
         {machines.map((machine, index) => (
           <div key={index} >
-            <h2>{machine}</h2>
+            <h2>{machine.name}</h2> {/* Assuming each machine object has a 'name' property */}
             <div className={style.buttonContainer}>
               <button>Book</button>
-
             </div>
           </div>
         ))}
